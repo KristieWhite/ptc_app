b2dfd2b98cc20a2fbdd3b0a4852d97d047fbcb0e
Kristie White <insanelynocturnal@gmail.com>
2015-12-08 16:06:26 -0800

    update

--- js/events.js
+++ js/events.js
@@ -8,7 +8,7 @@ var Router = Backbone.Router.extend({
 	},
 	routes: {
 		"eventsList": "eventsList",
-		"eventsDetail": "eventsDetail"
+		"eventsDetail": "eventsDetail"//fill in routers as pages are built
 	}
 });
 
@@ -17,17 +17,18 @@ var router = new Router();
 router.navigate("/");
 
 router.on('route:', function() {
-	$("eventsListDiv").show();
-	$("eventsDetailDiv").show();
+	$("#eventsListDiv").show();
+	$("#eventsDetailDiv").show();
 });
 
 var eventsModel = Backbone.Model.extend({
 	initialize: function() {
+		console.log("events model initialized");
 		defaults: {
-			"": null
-		},
+			event: null//place in proper names from api
+		}
 		Model: eventsModel,
-		url: ''
+		url: 'http://placeapiurlhere'//place api url here
 	}
 });
 
@@ -39,7 +40,7 @@ var eventsCollection = Backbone.Collection.extend({
 var events = new eventsCollection();
 events.fetch({
 	success: function(resp) {
-
+		console.log(resp);
 		var eventsInfo = {
 			'events': resp.toJSON()
 		};
