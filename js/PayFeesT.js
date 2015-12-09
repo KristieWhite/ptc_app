a6b73a96e2e56f9ebcac24f3ba84034f36809c68
Eijah Duane Prince <elijahprince73@gmail.com>
2015-12-09 14:46:13 -0800

    fh

---
+++ js/PayFeesT.js
@@ -0,0 +1,24 @@
+function addNewItem(list, itemtext) {
+	var listItem = document.createElement("li");
+	listItem.innerHTML = itemtext;
+
+	var list = document.getElementById("todoList");
+	list.appendChild(listItem);
+}
+
+var inItemText = document.getElementById("inItemText")
+inItemText.focus();
+
+inItemText.onkeyup = function (e) {
+	
+	if (e.which == 13)
+		var itemtext = inItemText.value;
+	if (!itemtext || itemtext == "" || itemtext == " ") {
+		return false;
+	}
+
+	addNewItem(document.getElementById("todoList"), itemtext);
+	
+	inItemText.focus();
+	inItemText.select();
+};
