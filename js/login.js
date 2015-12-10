67c3b185bbc858bbfb7306c10be2f6b4ba051065
Kristie White <insanelynocturnal@gmail.com>
2015-12-10 14:06:27 -0800

    log in js

--- js/login.js
+++ js/login.js
@@ -1,3 +1,49 @@
 $(document).ready(function($){
 
+$("#logInSubmit").on('submit', function(e) {
+	e.preventDefault();
+	var username = $("#username").val();
+	var password = $("#password").val();
+	$.ajax({
+		url: 'https://murmuring-sands-9831.herokuapp.com/api/api-token-auth/',
+		data: {username: username, password: password},
+		method: 'post'
+	}).then(function(resp) {
+		if ($('#remember_me').clicked == true) {
+			$.cookie('AuthToken', resp.token);
+			setToken(token);
+			User.set(resp.User)
+		}
+	})
 });
+
+var _sync = Backbone.sync;
+Backbone.sync = function(method, model, options) {
+	if (model && (method === 'create' || method === 'update' || method === 'patch')) {
+		options.contentType = 'application/json';
+		options.data = JSON.stringify(options.attrs || model.toJSON());
+	}
+
+	_.extend(options.data, {
+		"access_token": "some-token"
+	});
+	return _sync.call(this, method, model, options);
+}
+
+model.fetch().fail(/*redirect*/);
+
+$.ajaxSetup({
+	headers: {
+		"accept": "application/json",
+		"token": Your_Token
+	}
+});
+
+$.ajaxSend(function(event, request) {
+	var token = App.getAuthToken();
+	if (token) {
+		request.setRequestHeader("token", token);
+	}
+});
+
+});//closes document ready
---
+++ js/models.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+//login model
+
+var UserParent = Backbone.Model.extend ({
+	initialize: function() {
+		console.log("user parent model");
+	},
+	validate: function(attrs) {
+		if(!attrs.username) {
+			$("#usernameLogIn").html("username is required");
+			}
+			if (!attrs.password) {
+			$("#passwordLogIn").html("password is required");
+			}
+		}
+	});
+
+var model = new UserParent();
+
+
+
+});//closes document ready
