057b9b58a22dbf860ae58eb115953b369c554322
Jaimarie Velasco <jaimariev@Jaimaries-MacBook-Pro.local>
2015-12-10 14:08:31 -0800

    saving changes

--- js/login.js
+++ js/login.js
@@ -1,3 +1,17 @@
 $(document).ready(function($){
 
+  $("#logInSubmit").on('submit', function(e){
+    e.preventDefault();
+    var username = $("#username").val();
+    var password = $("#password").val();
+    $.ajax({
+      url: 'https://murmuring-sands-9831.herokuapp.com/api/api-token-auth/',
+      data: {username: username, password: password},
+      method: 'post'
+    }).then(function(resp) {
+      if($("#remember_me") === false) {
+
+      }
+    }
+  }
 });
