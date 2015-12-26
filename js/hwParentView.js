a28a03c808a0ce4d09fbc50cf7f5b3b2b5d8b7a0
Kristie White <insanelynocturnal@gmail.com>
2015-12-25 21:48:44 -0800

    parent hw detail

--- js/hwParentView.js
+++ js/hwParentView.js
@@ -4,51 +4,41 @@ $(document).ready(function () {
 		initialize: function () {
 			console.log("hwParentViewModel initialized");
 		},
-		defaults: {
-			name: null,
-			student: null,
-			title: null,
-			description: null,
-			image: null,
-			due_date: null,
-			points: "Not Graded Yet!"
-		},
+		// defaults: {
+		// 	name: null,
+		// 	student: null,
+		// 	title: null,
+		// 	description: null,
+		// 	image: null,
+		// 	due_date: null,
+		// 	points: "Not Graded Yet!"
+		// },
 		success: function () {
 			url: 'https://murmuring-sands-9831.herokuapp.com/api/students/#{id}/homework'
 		}
 	});
 
-	var HwParentViewCollection = Backbone.Model.extend({
+	var HwParentViewCollection = Backbone.Collection.extend({
 		Model: HwParentViewModel,
-		url: 'https://murmuring-sands-9831.herokuapp.com/api/students/#{id}homework'
+		url: 'https://murmuring-sands-9831.herokuapp.com/api/students/#{id}/homework'
 	});
 
 	var HomeworkAssigned = new HwParentViewCollection();
 
 	HomeworkAssigned.fetch({
+		data: $.param({ 
+			id: 13
+		}),
 		success: function (resp) {
 			console.log('success', resp.toJSON());
 			var homeworkListObj = {
-				"homework": resp.toJSON().results
+				"homework": resp.toJSON()[0].results
 			};
 			var assignmentListTemplate = $("#assignmentListTemplate").text();
 			var pHomeworkHTML = Mustache.render(assignmentListTemplate, homeworkListObj);
 			console.log("homeworkListObj", homeworkListObj);
 			//console.log("pHomeworkHTML", pHomeworkHTML);
 			$("#hwApiDiv").html(pHomeworkHTML);
-			function getURIParameter(param, asArray) {
-				return document.location.search.substring(1).split('&').reduce(function(p,c) {
-					var parts = c.split('=', 2).map(function(param) {
-						return decodeURIComponent(param);
-					});
-					if(parts.length == 0 || parts[0] != param) 
-						return (p instanceof Array) && !asArray ? null : p;
-						return asArray ? p.concat(parts.concat(true)[1]) : parts.concat(true)[1];
-					},
-					[]);
-			}
-			getURIParameter("id")
-			getURIParameter("id", true)
 		},
 		error: function (err) {
 			console.log('error homeworkList', err);
@@ -61,22 +51,28 @@ $(document).ready(function () {
 			console.log("hwDetailParentModel initialized");
 		},
 		success: function() {
-			url: 'https://murmuring-sands-9831.herokuapp.com/api/student_homework/#{id}'
+			url: 'https://murmuring-sands-9831.herokuapp.com/api/student_homework'
 		}
 	});
 
-	var HwDetailParentCollection = Backbone.Model.extend({
+	var HwDetailParentCollection = Backbone.Collection.extend({
 		Model: HwDetailParentModel,
-		url: 'https://murmuring-sands-9831.herokuapp.com/api/student_homework/#{id}'
+		url: 'https://murmuring-sands-9831.herokuapp.com/api/student_homework/#{id}',
+		parse: function(data) {
+			return data.student_homework;
+		}
 	});
 
 	var HomeworkDetailParent = new HwDetailParentCollection();
 
 	HomeworkDetailParent.fetch({
+		data: $.param({ 
+			id: 13
+		}),
 		success: function(resp) {
 			console.log('success', resp.toJSON());
 			var homeworkDetailObj = {
-				"student_homework": resp.toJSON().results
+				"student_homework": resp.toJSON()[0].results
 			};
 			var assignmentDetailTemplate = $("#assignmentDetailTemplate").text();
 			var pDetailHomeworkHTML = Mustache.render(assignmentDetailTemplate, homeworkDetailObj);
@@ -87,6 +83,7 @@ $(document).ready(function () {
 		error: function(err) {
 			console.log('error homeworkDetail', err);
 		}
+	
 	});
 
 });//document ready
